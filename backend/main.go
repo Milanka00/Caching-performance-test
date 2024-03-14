b4f0afca79f17c42d53c939be869ec587a68ef15
Milanka00 <milankatharangana01@gmail.com>
2024-03-14 13:14:10 +0530

    fixed /publiccache

--- backend/main.go
+++ backend/main.go
@@ -46,8 +46,8 @@ func NoCacheHandler(w http.ResponseWriter, r *http.Request) {
 func PublicCacheHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Cache-Control", "public, max-age=60")
     w.Write(payload)
-    additionalContent := []byte("cached as public")
-    w.Write(additionalContent)
+    // additionalContent := []byte("cached as public")
+    // w.Write(additionalContent)
     
 }
 
